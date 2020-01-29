7a26aef52f8c3080db3b92be4d0d341f3df98a70
Michelle He <mjhe@mit.edu>
2020-01-28 21:27:17 -0500

    tutorial and some styling

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,11 +3,13 @@ import GoogleLogin, { GoogleLogout } from "react-google-login";
 import Sky from "../modules/Sky.js"
 import Constellation from "../modules/Constellation.js"
 import NavBar from "../modules/NavBar.js"
+import Carousel from "./tutorial.js"
 
-// import {Slideshow} from "./slideshow.js"
 
 import "../../utilities.css";
 import "./Home.css";
+
+
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "201457722316-pqqtv96dl1ib3oschj9lde0l0f0mdlph.apps.googleusercontent.com";
@@ -23,19 +25,22 @@ class Home extends Component {
     // remember -- api calls go here!
   }
 
+  
+
   render() {
     return (
       <div className="u-scroll"> 
         <section className="whole">
         <section>
+          <div class="galaxy">
           <div className="main">
             <div className="title">
               ASTRA
             </div>
             <div className="quote">
-              reach for the stars
+              rewrite the stars
             </div>
-            {/* <img src="./nightsky.jpg" className="constellation-image"/>  */}
+          </div>
           </div>
         </section>
         </section>
@@ -43,12 +48,13 @@ class Home extends Component {
           <div className="instructions-block">
               <div className="instructions-title">
                 <br></br>
-                  tutorial
+                  Tutorial
               </div>
               <div className="instructions">
-                <div>
-                {/* {Slideshow()} */}
-                </div>
+              <div >
+                <Carousel
+                />
+              </div>
               </div>
           </div>
         </section>
